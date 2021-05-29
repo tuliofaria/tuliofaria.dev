6586b57c76f7670d0a0877366e8bf90259786379
Tulio Faria <tuliofaria@gmail.com>
2021-05-29 18:52:48 -0300

    fix purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,19 @@
 const purgecss = [
-  "@fullhuman/postcss-purgecss",
+  '@fullhuman/postcss-purgecss',
   {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
-    whitelist:['body', 'html'],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-  }
-];
+    content: ['./components/**/*.js', './pages/**/*.js'],
+    whitelist: ['body', 'html'],
+    whitelistPatterns: [/psv-/],
+    whitelistPatternsChildren: [/psv-/],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+]
 
 module.exports = {
-    plugins: [
-      "postcss-import",
-      "tailwindcss",
-      "autoprefixer",
-      ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
-    ]
-  };
+  plugins: [
+    'postcss-import',
+    'tailwindcss',
+    'autoprefixer',
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
+}
